e2469c66c94b8f3cbcfe31ca9224ec0f6a60c809
Reuben Avery <ravery@bitswarm.io>
2016-06-01 15:12:50 -0400

    wrangling

--- manifests/aws/cli.pp
+++ manifests/aws/cli.pp
@@ -1,12 +1,12 @@
 class bsl_infrastructure::aws::cli {
   notify { '#### here we are in bsl_infrastructure::aws::cli': }
 
-  include '::python'
+  contain '::python'
+  include '::awscli'
 
   anchor { 'bsl_infrastructure::aws::cli::begin': }
   ->Class['::python::install']
-  ->Package <| title == 'pip' |>
-  ->class { '::awscli': }
+  ->Class['::awscli']
   ->anchor { 'bsl_infrastructure::aws::cli::end': }
 
   # awscli::profile { 'default':
