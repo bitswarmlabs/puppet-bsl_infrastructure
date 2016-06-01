0e78db964c884f942392ad891bdeb8219d8c3ce0
Reuben Avery <ravery@bitswarm.io>
2016-06-01 14:38:36 -0400

    awscli

--- manifests/aws.pp
+++ manifests/aws.pp
@@ -2,9 +2,10 @@ class bsl_infrastructure::aws {
   include '::python'
   include 'bsl_infrastructure::aws::sdk'
 
-  Class['::python']->
-  class { '::ec2tagfacts':
-    
-  }
+  class { '::awscli': }
 
+  # awscli::profile { 'default':
+  #   aws_access_key_id     => 'MYAWSACCESSKEYID',
+  #   aws_secret_access_key => 'MYAWSSECRETACESSKEY'
+  # }
 }
