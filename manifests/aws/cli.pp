8c90633d3c70d46110ee306ead78c653285180e6
Reuben Avery <ravery@bitswarm.io>
2016-06-01 15:33:37 -0400

    wrangling

--- manifests/aws/cli.pp
+++ manifests/aws/cli.pp
@@ -1,14 +1,15 @@
-class bsl_infrastructure::aws::cli {
+class bsl_infrastructure::aws::cli(
+  $version = 'latest',
+) {
   notify { '#### here we are in bsl_infrastructure::aws::cli': }
 
-  # contain '::python'
-  include '::awscli'
+  include '::python'
 
-  anchor { 'bsl_infrastructure::aws::cli::begin': }
-  # ->Class['::python::install']
-  ->Class['::awscli']
-  ->anchor { 'bsl_infrastructure::aws::cli::end': }
-
+  package { 'awscli':
+    ensure   => $version,
+    provider => 'pip',
+  }
+  
   # awscli::profile { 'default':
   #   aws_access_key_id     => 'MYAWSACCESSKEYID',
   #   aws_secret_access_key => 'MYAWSSECRETACESSKEY'
