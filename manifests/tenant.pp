bac5a2049a81d7f5cf9d29cc1e0f78c1c65982cc
Reuben Avery <ravery@bitswarm.io>
2016-06-03 14:10:52 -0400

    tenant class

---
+++ manifests/tenant.pp
@@ -0,0 +1,12 @@
+define bsl_infrastructure::tenant(
+  $account_id = $::bsl_account_id,
+  $internal_domain = $::domain,
+  $providers = [],
+) {
+  $defaults = {
+    account_id => $account_id,
+    internal_domain => $internal_domain,
+  }
+
+  create_resources('bsl_infrastructure::provider', $providers, $defaults)
+}
