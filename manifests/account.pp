bfa3ccfea5dbe0f5831c6f434179da7176fdb30d
Reuben Avery <ravery@bitswarm.io>
2016-06-03 14:16:10 -0400

    debugging

--- manifests/account.pp
+++ manifests/account.pp
@@ -1,11 +1,13 @@
 define bsl_infrastructure::account(
   $account_id = $::bsl_account_id,
   $internal_domain = $::domain,
+  $puppetmaster = hiera('puppetmaster', 'puppet'),
   $providers = [],
 ) {
   $defaults = {
     tenant_id => $account_id,
     internal_domain => $internal_domain,
+    puppetmaster => $puppetmaster,
   }
 
   create_resources('bsl_infrastructure::tenant', $providers, $defaults)
--- manifests/tenant.pp
+++ manifests/tenant.pp
@@ -1,11 +1,13 @@
 define bsl_infrastructure::tenant(
   $account_id = $::bsl_account_id,
   $internal_domain = $::domain,
+  $puppetmaster = hiera('puppetmaster', 'puppet'),
   $providers = [],
 ) {
   $defaults = {
     account_id => $account_id,
     internal_domain => $internal_domain,
+    puppetmaster => $puppetmaster,
   }
 
   create_resources('bsl_infrastructure::provider', $providers, $defaults)
