c5f74c0ffb31b5e18c01738b3f58eef4276dc94b
Reuben Avery <ravery@bitswarm.io>
2016-06-05 15:45:14 -0400

    dependency wrangling

--- manifests/aws/resource/vpc.pp
+++ manifests/aws/resource/vpc.pp
@@ -64,11 +64,11 @@ define bsl_infrastructure::aws::resource::vpc(
     }
   }
 
-  if $ensure == absent {
-    Ec2_vpc_internet_gateway[$name]
-    ~> Ec2_vpc_subnet<| |>
-    ~> Ec2_vpc_routetable<| |>
-    ~> Ec2_vpc[$name]
-    ~> Ec2_vpc_dhcp_options[$name]
-  }
+  # if $ensure == absent {
+  #   Ec2_vpc_internet_gateway[$name]
+  #   ~> Ec2_vpc_subnet<| |>
+  #   ~> Ec2_vpc_routetable<| |>
+  #   ~> Ec2_vpc[$name]
+  #   ~> Ec2_vpc_dhcp_options[$name]
+  # }
 }
