955a6f54808229ab337c000a73bdb8b93018a59a
Reuben Avery <ravery@bitswarm.io>
2016-06-06 17:10:14 -0400

    debugging

--- manifests/debug.pp
+++ manifests/debug.pp
@@ -6,33 +6,33 @@ class bsl_infrastructure::debug(
   if $ensure == absent {
     # ec2_securitygroup { 'sample-sg':
     #   ensure   => absent,
-    #   region => 'sa-east-1',
+    #   region => 'us-east-1',
     # } ~>
 
     ec2_vpc_internet_gateway { 'sample-igw':
       ensure => absent,
-      region => 'sa-east-1',
+      region => 'us-east-1',
     } ~>
 
     ec2_vpc_subnet { 'sample-subnet':
       ensure => absent,
-      region => 'sa-east-1',
+      region => 'us-east-1',
     } ~>
 
     ec2_vpc_routetable { 'sample-routes':
       ensure => absent,
-      region => 'sa-east-1',
+      region => 'us-east-1',
     } ~>
 
     ec2_vpc { 'sample-vpc':
       ensure => absent,
-      region => 'sa-east-1',
+      region => 'us-east-1',
     }
     # ~>
     #
     # ec2_vpc_dhcp_options { 'sample-options':
     #   ensure => absent,
-    #   region => 'sa-east-1',
+    #   region => 'us-east-1',
     # }
 
   }
