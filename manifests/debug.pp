676fa4bf25b040e41ae481020c7514ef77a7c9e6
Reuben Avery <ravery@bitswarm.io>
2016-06-06 16:57:55 -0400

    debugging

--- manifests/debug.pp
+++ manifests/debug.pp
@@ -6,28 +6,22 @@ class bsl_infrastructure::debug {
     region       => 'us-east-1',
     cidr_block   => '172.30.0.0/16',
   }
-  ~>
 
-  # ec2_securitygroup { 'sample-sg':
-  #   ensure      => present,
-  #   region      => 'us-east-1',
-  #   vpc         => 'sample-vpc',
-  #   description => 'Security group for VPC',
-  #   ingress     => [{
-  #     security_group => 'sample-sg',
-  #   },{
-  #     protocol => 'tcp',
-  #     port     => 22,
-  #     cidr     => '0.0.0.0/0'
-  #   }]
-  # }
+  ec2_vpc_subnet { 'sample-subnet':
+    ensure            => present,
+    region            => 'us-east-1',
+    vpc               => 'sample-vpc',
+    cidr_block        => '172.30.0.0/24',
+    availability_zone => 'us-east-1b',
+    route_table       => 'sample-routes',
+  }
 
   ec2_vpc_internet_gateway { 'sample-igw':
     ensure => present,
     region => 'us-east-1',
     vpc    => 'sample-vpc',
   }
-  ~>
+
   ec2_vpc_routetable { 'sample-routes':
     ensure => present,
     region => 'us-east-1',
@@ -41,14 +35,5 @@ class bsl_infrastructure::debug {
       #   gateway                => 'local'
       # },
     ],
-  }
-  ~>
-  ec2_vpc_subnet { 'sample-subnet':
-    ensure            => present,
-    region            => 'us-east-1',
-    vpc               => 'sample-vpc',
-    cidr_block        => '172.30.0.0/24',
-    availability_zone => 'us-east-1b',
-    route_table       => 'sample-routes',
   }
 }
