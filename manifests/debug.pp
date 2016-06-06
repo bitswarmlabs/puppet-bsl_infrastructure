cbadc006de01e2ca2f75e28ed5e41f936145b6cc
Reuben Avery <ravery@bitswarm.io>
2016-06-06 17:07:43 -0400

    debugging

--- manifests/debug.pp
+++ manifests/debug.pp
@@ -43,8 +43,9 @@ class bsl_infrastructure::debug(
   if $ensure == absent {
     Ec2_vpc_subnet['sample-subnet']~>
     Ec2_vpc_internet_gateway['sample-igw']~>
-    Ec2_vpc_routetable['sample-routes']~>
-    Ec2_vpc['sample-vpc']
+    Ec2_vpc_routetable['sample-routes']
+    # ~>
+    # Ec2_vpc['sample-vpc']
     # ~>
     # Ec2_vpc_dhcp_options['sample-options']
   }
