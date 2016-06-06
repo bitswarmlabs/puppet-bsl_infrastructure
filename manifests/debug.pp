452cb13e3402d279280c18cc3b53ad7cce0c2a4e
Reuben Avery <ravery@bitswarm.io>
2016-06-06 09:36:34 -0400

    changing region

--- manifests/debug.pp
+++ manifests/debug.pp
@@ -3,13 +3,13 @@ class bsl_infrastructure::debug {
 
   ec2_vpc { 'sample-vpc':
     ensure       => present,
-    region       => 'sa-east-1',
+    region       => 'us-east-1',
     cidr_block   => '172.30.0.0/16',
   }
 
   # ec2_securitygroup { 'sample-sg':
   #   ensure      => present,
-  #   region      => 'sa-east-1',
+  #   region      => 'us-east-1',
   #   vpc         => 'sample-vpc',
   #   description => 'Security group for VPC',
   #   ingress     => [{
@@ -23,22 +23,22 @@ class bsl_infrastructure::debug {
 
   ec2_vpc_subnet { 'sample-subnet':
     ensure            => present,
-    region            => 'sa-east-1',
+    region            => 'us-east-1',
     vpc               => 'sample-vpc',
     cidr_block        => '172.30.0.0/24',
-    availability_zone => 'sa-east-1a',
+    availability_zone => 'us-east-1b',
     route_table       => 'sample-routes',
   }
 
   ec2_vpc_internet_gateway { 'sample-igw':
     ensure => present,
-    region => 'sa-east-1',
+    region => 'us-east-1',
     vpc    => 'sample-vpc',
   }
 
   ec2_vpc_routetable { 'sample-routes':
     ensure => present,
-    region => 'sa-east-1',
+    region => 'us-east-1',
     vpc    => 'sample-vpc',
     routes => [
       {
