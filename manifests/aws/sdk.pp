3d6a6566bfb1c632b4c250699305cf558c056f61
Reuben Avery <ravery@bitswarm.io>
2016-06-06 16:04:31 -0400

    dropping aws sdk down to 2.2.37

--- manifests/aws/sdk.pp
+++ manifests/aws/sdk.pp
@@ -1,5 +1,5 @@
 class bsl_infrastructure::aws::sdk(
-  $aws_sdk_gem_version = latest, #'v2.3.11'
+  $aws_sdk_gem_version = 'v2.2.37', #latest, #'v2.3.11'
   $retries_gem_version = present,
 ) {
   # notify { '#### here we are in bsl_infrastructure::aws::sdk': }
