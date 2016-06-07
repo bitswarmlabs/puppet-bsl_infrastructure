6143e0fe84d43c57e11bd3cbbf6a0a7d9fee2e43
Reuben Avery <ravery@bitswarm.io>
2016-06-07 14:21:10 -0400

    route53 haking

--- manifests/aws/resource/route53/zone.pp
+++ manifests/aws/resource/route53/zone.pp
@@ -2,11 +2,27 @@ define bsl_infrastructure::aws::resource::route53::zone(
   $ensure = 'present',
   $account_id = hiera('bsl_account_id', $::bsl_account_id),
   $tenant_id = hiera('bsl_tenant_id', $::bsl_tenant_id),
+  $comment = '',
+  $type = 'private',
   $vpcs = {},
 ) {
   include 'bsl_infrastructure::aws::resource::route53'
 
+  $name_normalized = regsubst($name, '(([\w]+\.)*[\w]+)', '$&.')
+  validate_re($name_normalized, '^((([\w]+\.)*[\w]+)\.)$')
+  
+  # TODO : the below only creates a public zone currently, also cannot associate with VPC, as $name is the only
+  # implemented parameter.
+  #
   route53_zone { $name:
-    ensure => $ensure,
+    name => $name_normalized,
   }
+  #
+  # needs to support:
+  # route53_zone { $name:
+  #   type => $type,
+  #   comment => $comment,
+  #   vpcs => $vpcs,
+  #   ensure => $ensure,
+  # }
 }
