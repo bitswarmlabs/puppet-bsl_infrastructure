72d06485e6e4e106ae9d93be691f9c533e213b88
Reuben Avery <ravery@bitswarm.io>
2016-06-08 17:27:05 -0400

    debugging

--- manifests/aws/resource/vpc/security_group.pp
+++ manifests/aws/resource/vpc/security_group.pp
@@ -26,12 +26,13 @@ define bsl_infrastructure::aws::resource::vpc::security_group(
   validate_hash($tags)
   $all_tags = merge($default_tags, $tags)
 
-  # ec2_securitygroup { $name:
-  #   ensure      => $ensure,
-  #   region      => $region,
-  #   description => $description,
-  #   ingress     => $ingress,
-  #   vpc         => $vpc,
-  #   tags        => $all_tags,
-  # }
+  ec2_securitygroup { $title:
+    ensure      => $ensure,
+    name        => $name,
+    region      => $region,
+    description => $description,
+    ingress     => $ingress,
+    vpc         => $vpc,
+    tags        => $all_tags,
+  }
 }
